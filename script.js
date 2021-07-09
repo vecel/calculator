789022f2381df2d684e43775619c9776a975250f
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-09 12:36:19 +0200

    dom objects

---
+++ script.js
@@ -0,0 +1,8 @@
+const lastOperationDisplay = document.querySelector('.previous-operation');
+const currentOperationDisplay = document.querySelector('.current-operation');
+
+const numberButtons = document.querySelectorAll('.calc-button.number');
+const operatorButtons = document.querySelectorAll('.calc-button.operator');
+const clearButton = document.querySelector('.calc-button.clear');
+const equalButton = document.querySelector('.calc-button.equals');
+
